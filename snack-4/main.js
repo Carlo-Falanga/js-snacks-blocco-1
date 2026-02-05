eef7248a37f92f807543e8d9d004d00821ff227d
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-05 13:00:13 +0100

    complete 4th exercise

--- snack-4/main.js
+++ snack-4/main.js
@@ -1,1 +1,13 @@
 console.log('It works');
+
+let array = []
+
+for (let i = 0; i < 6; i++){
+   const number = Number(prompt("Insert a number"));
+   console.log(number);
+
+   if (number % 2 !== 0){
+    array.push(number)  
+} } 
+
+console.log('Numeri dispari inseriti:', array);
