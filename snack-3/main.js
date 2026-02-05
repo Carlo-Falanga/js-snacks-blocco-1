d0ba33ac6fd7ada34a7f7339f5567f95cfdc0788
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-05 12:35:27 +0100

    general fix

--- snack-3/main.js
+++ snack-3/main.js
@@ -4,8 +4,8 @@ console.log('It works');
 let sum = 0;
 
 for (let i = 0; i < 10; i++){
-    const firstNumber = Number(prompt("Insert a number"));
-    sum = sum + firstNumber
+    const number = Number(prompt("Insert a number"));
+    sum = sum + number
 }
 
 alert(sum)
