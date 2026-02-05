28fde5fe0363a470c4dc4f306c739b69108b9f6b
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-05 12:07:24 +0100

    complete exercise 3

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,1 +1,11 @@
 console.log('It works');
+
+
+let sum = 0;
+
+for (let i = 0; i < 10; i++){
+    const firstNumber = Number(prompt("Insert a number"));
+    sum = sum + firstNumber
+}
+
+alert(sum)
